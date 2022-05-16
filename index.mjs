34a69fabbf204f114e24633957d1c519de7e79d7
Kev2994 <kevinvirani5@gmail.com>
2022-05-15 22:53:04 -0400

    fourth commit with endpoint

--- index.mjs
+++ index.mjs
@@ -2,15 +2,21 @@ import express from "express";
 import { router } from "./routes/allMakes.mjs";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { Vehicle } from "./model/vehicleModel.mjs";
 import cron from "node-cron";
 import { getProcessedData } from "./scheduleTask.mjs";
+import { ApolloServer, gql } from "apollo-server";
+import { Vehicle } from "./model/vehicleModel.mjs";
 
 dotenv.config();
 
+var today = new Date();
+
 const app = express();
 
-const task = cron.schedule("0 */5 * * * *", getProcessedData);
+const task = cron.schedule(
+  `${today.getMinutes() + 1} ${today.getHours()}} * * *`,
+  getProcessedData
+);
 
 task.start();
 
--- scheduleTask.mjs
+++ scheduleTask.mjs
@@ -5,37 +5,30 @@ const getProcessedData = async (req, res) => {
   try {
     console.log("cron started running");
     let makes = await fetchAllMakes();
-    makes = makes.slice(0, 10);
+    makes = makes.slice(0, 50);
+    let position = 0;
+    // let results = [];
+    while (position < makes.length) {
+      const itemsForBatch = makes.slice(position, position + 10);
+      let fetchedData = await Promise.all(
+        itemsForBatch.map(async (make) => {
+          make.vehicleTypes = await fetchMake(make.Make_ID[0]);
+          return make;
+        })
+      );
 
-    const batchOperation = async () => {
-      try {
-        let position = 0;
-        // let results = [];
-        while (position < makes.length) {
-          const itemsForBatch = makes.slice(position, position + 10);
-          let fetchedData = await Promise.all(
-            itemsForBatch.map(async (make) => {
-              make.vehicleTypes = await fetchMake(make.Make_ID[0]);
-              return make;
-            })
-          );
-
-          await Vehicle.bulkWrite(
-            fetchedData.map((data) => ({
-              updateOne: {
-                filter: { Make_ID: data.Make_ID },
-                update: data,
-                upsert: true,
-              },
-            }))
-          );
-          //results = [...results, ...fetchedData];
-          position += 10;
-        }
-      } catch (err) {
-        console.error(err);
-      }
-    };
+      await Vehicle.bulkWrite(
+        fetchedData.map((data) => ({
+          updateOne: {
+            filter: { Make_ID: data.Make_ID },
+            update: data,
+            upsert: true,
+          },
+        }))
+      );
+      //results = [...results, ...fetchedData];
+      position += 10;
+    }
   } catch (err) {
     console.error(err);
   }
